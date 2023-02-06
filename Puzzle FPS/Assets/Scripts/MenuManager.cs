e4a5f97a371b6cfded8304c34044230ba942b8f0
Gimzad <pallottanicholas@hotmail.com>
2023-02-05 21:20:12 -0500

    Initial Menu Manager Script
    Basic Menu Manager accounting for opening and closing menus, returning to previous ones, and handling an in-game menu panel as well.

---
+++ Puzzle FPS/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+	[Header("UI Panels")]
+    public GameObject MainMenuPanel;
+	public GameObject PlayerSettingsPanel;
+	public GameObject GameMenuPanel;
+
+    #region Internal Variables
+    private GameObject previousMenuPanel;
+	private GameObject activeMenuPanel;
+	#endregion
+	//Bool for determining if changes made in a menu will be updated to the script,
+	//or if changes to the numbers, sliders, etc. should revert to their assigned values;
+	private bool changesSaved;
+
+	[Header("Menu States")]
+	//Handles toggle for the in-game menu
+	[SerializeField]
+	private bool canToggleGameMenu;
+	[SerializeField]
+	private bool gameMenuIsUp;
+
+    #region Menu Access Methods
+
+	public bool CanToggleGameMenu()
+    {
+		return canToggleGameMenu;
+    }
+	public bool GameMenuIsUp()
+	{
+		return gameMenuIsUp;
+	}
+	#endregion
+
+	#region Global Menu Methods
+	public void DeactivateAllMenus()
+    {
+		if (MainMenuPanel.activeInHierarchy)
+			MainMenuPanel.SetActive(false);
+		if (PlayerSettingsPanel.activeInHierarchy)
+			PlayerSettingsPanel.SetActive(false);
+		if (GameMenuPanel.activeInHierarchy)
+			GameMenuPanel.SetActive(false);	
+	}
+	public void OpenPreviousMenuPanel()
+	{
+		if (activeMenuPanel == PlayerSettingsPanel && !changesSaved)
+        {
+			RevertMenuSettingsFromScript();
+        }
+        else
+        {
+			AssertMenuSettingsToScript();
+        }
+		//Temp object for active menu
+		GameObject panelHolder;
+
+		panelHolder = activeMenuPanel;
+		activeMenuPanel.gameObject.SetActive(false);
+
+		activeMenuPanel = previousMenuPanel;
+		activeMenuPanel.gameObject.SetActive(true);
+
+		previousMenuPanel = panelHolder;
+	}
+    #endregion
+    #region Internal Menu Mehods
+    private void DisplayMenuPanel(GameObject menuPanel)
+	{
+		if (activeMenuPanel != null)
+			previousMenuPanel = activeMenuPanel;
+
+		menuPanel.gameObject.SetActive(true);
+		activeMenuPanel = menuPanel;
+	}
+	private void CloseActiveMenuPanel()
+	{
+		previousMenuPanel = activeMenuPanel;
+		activeMenuPanel.gameObject.SetActive(false);
+		activeMenuPanel = null;
+	}
+	#endregion
+	#region Settings Menu Methods
+	private void RevertMenuSettingsFromScript()
+	{
+		//Will reset sliders, text, numbers, etc. back to the un-changed settings in the script
+	}
+	private void AssertMenuSettingsToScript()
+    {
+		//Will take the sliders, texts, numbers, etc. and assign those settings to the variable in the script
+    }
+	#endregion
+	#region Specific Menu Methods
+	public void DisplayMainMenu()
+	{
+		DisplayMenuPanel(MainMenuPanel);
+		//Ensure in-game menu toggling is disabled while main menu is up.
+		canToggleGameMenu = false;
+	}
+
+	public void DisplayPlayerSettingsMenu()
+	{
+		DisplayMenuPanel(PlayerSettingsPanel);
+		//Ensure in-game menu toggling is disabled while main menu is up.
+		canToggleGameMenu = false;
+		//Settings menu is up, so prepare boolean for change tracking
+		changesSaved = false;
+	}
+
+    #endregion
+    #region In-Game Menu Methods
+    public void DisplayGameMenu()
+	{
+		//Close existing menu if up
+		CloseActiveMenuPanel();
+		DisplayMenuPanel(GameMenuPanel);
+		//Ensure toggle of game menu is enabled
+		canToggleGameMenu = true;
+		//Enable bool for in-game menu
+		gameMenuIsUp = true;
+	}
+	public void CloseGameMenu()
+	{
+		//Only if the game menu is active
+		if (GameMenuPanel.gameObject.activeInHierarchy == true)
+		{
+			CloseActiveMenuPanel();
+			//Ensure toggle of game menu is enabled
+			canToggleGameMenu = true;
+			//Disable bool for in-game menu
+			gameMenuIsUp = false;
+		}
+	}
+	#endregion
+}
