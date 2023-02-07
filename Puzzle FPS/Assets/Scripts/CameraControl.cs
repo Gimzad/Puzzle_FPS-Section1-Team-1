bd78930891748e63d535fcfcd6676b15839a2e3f
MrFoxFire <johnapatricelli@gmail.com>
2023-02-07 14:12:16 -0500

    Update CameraControl.cs

--- Puzzle FPS/Assets/Scripts/CameraControl.cs
+++ Puzzle FPS/Assets/Scripts/CameraControl.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
+
+    [SerializeField] int sensHor;
+    [SerializeField] int sensVer;
+
+    [SerializeField] int lockVerMin;
+    [SerializeField] int lockVerMax;
+
+    [SerializeField] bool invertX;
+
+    float xRotation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //get input
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensVer;
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensHor;
+
+        if (invertX)
+        {
+            xRotation += mouseY;
+        }
+        else
+        {
+            xRotation -= mouseY;
+        }
+
+
+        //clamp the camera rotation
+        xRotation = Mathf.Clamp(xRotation, lockVerMin, lockVerMax);
+
+
+        //rotate the camera on the X-axis
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        //rotate the player on its Y-axis
+        transform.parent.Rotate(Vector3.up * mouseX);
+
+
+
     }
 }
