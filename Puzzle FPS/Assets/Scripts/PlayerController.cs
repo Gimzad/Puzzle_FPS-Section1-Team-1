7b9e3544c613f8617ccca4ab5180549e2b462c31
MikeyBurgess9142 <MJBurgess@student.fullsail.edu>
2023-02-08 16:49:15 -0500

    basic movement begun

--- Puzzle FPS/Assets/Scripts/PlayerController.cs
+++ Puzzle FPS/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
 
@@ -11,7 +11,8 @@ public class playerController : MonoBehaviour
     [SerializeField] int jumpSpeed;
     [SerializeField] int gravity;
 
-
+    Vector3 move;
+    Vector3 playerVelocity;
 
     // Start is called before the first frame update
     void Start()
@@ -23,5 +24,11 @@ public class playerController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void movement()
+    {
+        move = (transform.right * Input.GetAxis("Horizontal") + (transform.forward * Input.GetAxis("Vertical")));
+
     }
 }
