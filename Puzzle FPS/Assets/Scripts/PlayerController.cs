194385d7ff1805788cea0ea21aafc153f1b546de
MikeyBurgess9142 <MJBurgess@student.fullsail.edu>
2023-02-08 15:57:46 -0500

    set up player stats

--- Puzzle FPS/Assets/Scripts/PlayerController.cs
+++ Puzzle FPS/Assets/Scripts/PlayerController.cs
@@ -2,8 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class playerController : MonoBehaviour
 {
+    [SerializeField] CharacterController controller;
+
+    [SerializeField] int playerSpeed;
+    [SerializeField] int jumpTimes;
+    [SerializeField] int jumpSpeed;
+    [SerializeField] int gravity;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
