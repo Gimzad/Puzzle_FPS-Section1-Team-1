ee64e42fecd9fc72e8a06b3662debfe2665f2283
MrFoxFire <johnapatricelli@gmail.com>
2023-02-16 18:24:12 -0500

    DeathPlane functionality
    Scripted the death plane functionality

--- Puzzle FPS/Assets/Scripts/DeathPlane.cs
+++ Puzzle FPS/Assets/Scripts/DeathPlane.cs
@@ -18,9 +18,18 @@ public class DeathPlane : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (isDeathPlane && playerInZone)
+        {
+            ActivateDeathPlane();
+        }
+        //WIP 
+        else if (playerInZone)
+        {
+            StartCoroutine(ActivateStageHazard());
+        }
     }
 
+    //detects player collided with plane
     void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
@@ -29,12 +38,21 @@ public class DeathPlane : MonoBehaviour
         }
     }
 
+    //Function to kill the player upon emntry of the death plane
     void ActivateDeathPlane()
     {
-
+        GameManager.Instance.PlayerScript().TakeDamage(GameManager.Instance.PlayerScript().HP);
     }
-    void ActivateDamageHazard()
+
+    //function to be added to make a damage hazard instead of a kill plane WIP
+    IEnumerator ActivateStageHazard()
     {
+        do
+        {
+            GameManager.Instance.PlayerScript().TakeDamage(1);
+            yield return new WaitForSeconds(2.5f);
 
+        } while (playerInZone);
     }
+
 }
